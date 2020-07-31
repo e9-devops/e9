7277e38ccd804a4dd5d80b1e32721ae4d2e8ed00
Soumya Kanthi <soumya@e9ine.com>
2020-07-31 17:21:25 +0530

    chore: updated package.json version

--- js/app~381999ac.651ba709.js
+++ js/app~381999ac.651ba709.js
@@ -1,8 +1,1 @@
-(window['webpackJsonp'] = window['webpackJsonp'] || []).push([
-    ['app~381999ac'],
-    [
-        function (p, n, o) {
-            p.exports = o('56d7');
-        },
-    ],
-]);
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["app~381999ac"],[function(p,n,o){p.exports=o("56d7")}]]);
